e3fbc53029c148c859cc9b344883545362e857c0
Sakib Bin Alim <sakibbinalim@gmail.com>
2023-11-27 16:14:13 +0600

    Added Firebase to the project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shonglap/pages/login_page.dart';
 import 'package:shonglap/pages/register_page.dart';
+import 'package:shonglap/services/auth/auth_gate.dart';
 import 'package:shonglap/services/auth/login_or_register.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -14,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginOrRegister(),
+      home: AuthGate(),
     );
   }
 }
---
+++ lib/services/auth/auth_service.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  // sign in user
+
+  //sign out user
+}
