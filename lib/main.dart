21e8907e5e02523cf4230d94555679b49b913ac0
Sakib Bin Alim <sakibbinalim@gmail.com>
2023-11-29 17:20:27 +0600

    - Fixed the Firebase Firestore issue on android. - Configured using Flutterfire configure.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shonglap/firebase_options.dart';
 import 'package:shonglap/pages/login_page.dart';
 import 'package:shonglap/pages/register_page.dart';
 import 'package:shonglap/services/auth/auth_gate.dart';
@@ -9,7 +10,9 @@ import 'package:shonglap/services/auth/login_or_register.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     ChangeNotifierProvider(
       create: (context) => AuthService(),
